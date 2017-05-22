ce2fc1bbc0d808ba3ba7ad6a9fa5dcc3b1ca460c
Bruno Lajoie <brunolajoie@gmail.com>
2017-05-22 16:28:13 +0200

    updated school model and db schema with detailed adresses

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,6 @@
 class School < ApplicationRecord
   has_many :users
+  has_many :toys, through: :users
   validates :name, presence: true, uniqueness: { scope: :zipcode,
     message: "this school already exists" }
   validates :address, presence: true
