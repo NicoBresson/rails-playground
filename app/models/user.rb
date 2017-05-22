c11db2b27172581b589691a78da9e2d0d0556f37
Bruno Lajoie <brunolajoie@gmail.com>
2017-05-22 17:19:58 +0200

    finalised model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :school
+  has_many :toys, dependent: :destroy
+  has_many :transactions
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
