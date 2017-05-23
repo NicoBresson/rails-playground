03eae61b213f12cd513ada05a9a74fc285c76f09
Bruno Lajoie <brunolajoie@gmail.com>
2017-05-23 15:42:13 +0200

    fixed login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
-
+    # TODO: do NOT redirect to edit_user if we already have a school!
     if user.persisted?
       sign_in user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-      redirect_to edit_user_path(current_user)
+      if current_user.school_id
+        flash[:notice] = "Login successfull" if is_navigational_format?
+        redirect_to root_path
+      else
+        flash[:alert] = "Welcome! Please chose your school" if is_navigational_format?
+        redirect_to edit_user_path(current_user)
+      end
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
