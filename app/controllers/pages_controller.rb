d2d5476e302494015ce52b6b632789d1c53bd784
Bruno Lajoie <brunolajoie@gmail.com>
2017-05-25 13:45:29 +0200

    working find my school bar"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,15 @@ class PagesController < ApplicationController
       marker.infowindow render_to_string(partial: "/pages/school_map_box", locals: { school: school })
     end
   end
+
+  def search
+    @address = params[:search]
+    @schools_near = School.near(@address, 1)
+    @hash = Gmaps4rails.build_markers(@schools_near) do |school, marker|
+      marker.lat school.latitude
+      marker.lng school.longitude
+      marker.infowindow render_to_string(partial: "/pages/school_map_box", locals: { school: school })
+    end
+    # TODO: Try to redirect to /search#map-container
+  end
 end
